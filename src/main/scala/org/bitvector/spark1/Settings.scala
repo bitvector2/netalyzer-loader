01ff4c6ea8b6ccbcea1922f841f10724b313ba51
Steven Logue <bitvector2@gmail.com>
2016-03-24 14:23:39 -0700

    :lipstick:

--- src/main/scala/org/bitvector/spark1/Settings.scala
+++ src/main/scala/org/bitvector/spark1/Settings.scala
@@ -4,11 +4,7 @@ import com.typesafe.config.ConfigFactory
 
 class Settings() {
   val config = ConfigFactory.load()
-
-  // validate vs. reference.conf
   config.checkValid(ConfigFactory.defaultReference(), "examples")
-
-  // non-lazy fields, we want all exceptions at construct time
   val foo = config.getString("examples.foo")
   val bar = config.getInt("examples.bar")
 }
