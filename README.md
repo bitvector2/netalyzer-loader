d03785c7894ea31be631bf5e812d2acd016d82a5
Steven Logue <bitvector2@gmail.com>
2016-04-05 16:14:53 -0700

    beginning to add transforms to add throughput and utilization

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ From:  https://azure.microsoft.com/en-us/documentation/articles/hdinsight-apache
 curl -k --user "user:pass" -H 'Content-Type: application/json' -X POST -d '{ "file":"wasb://data@netalyzerdata.blob.core.windows.net/tmp/spark1-1.0-SNAPSHOT-all.jar", "className":"org.bitvector.spark1.Main" }' "https://netalyzer.azurehdinsight.net/livy/batches"
 
 curl -k --user "user:pass" -X GET "https://netalyzer.azurehdinsight.net/livy/batches/0"
+
--- src/main/scala/org/bitvector/spark1/Main.scala
+++ src/main/scala/org/bitvector/spark1/Main.scala
@@ -1,8 +1,11 @@
 package org.bitvector.spark1
 
 import org.apache.log4j.Logger
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.types._
 import org.apache.spark.{SparkConf, SparkContext}
+
 
 object Main {
   val settings = new Settings()
@@ -15,15 +18,41 @@ object Main {
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-    val df = sqlContext.read
+    // https://docs.oracle.com/javase/7/docs/api/java/sql/Timestamp.html
+    val customSchema = StructType(Array(
+      StructField("Timestamp", TimestampType, nullable = false),
+      StructField("Hostname", StringType, nullable = false),
+      StructField("PortName", StringType, nullable = false),
+      StructField("PortBitsPerSecond", LongType, nullable = false),
+      StructField("RxTotalBytes", LongType, nullable = false),
+      StructField("TxTotalBytes", LongType, nullable = false)
+    ))
+
+    // https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+    // https://github.com/databricks/spark-csv
+    val rawDf = sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "true")
-      .option("inferSchema", "true")
+      .option("dateFormat", "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+      .schema(customSchema)
       .load("wasb:///tmp/test_data.csv")
 
-    df.printSchema()
-    df.show()
+    val cookedDf = rawDf
+      .transform(addThroughput)
+      .transform(addUtilization)
+
+    cookedDf.show()
 
     logger.info("Stopping...")
+  }
+
+  def addThroughput(df: DataFrame): DataFrame = {
+    val windowsSpec = Window.partitionBy("Hostname", "PortName").orderBy("Timestamp").rowsBetween(-1, 0)
+    df
+  }
+
+  def addUtilization(df: DataFrame): DataFrame = {
+    val windowsSpec = Window.partitionBy("Hostname", "PortName").orderBy("Timestamp").rowsBetween(-1, 0)
+    df
   }
 }
